2f02f7f8bb0a630613bd26ae3a8706ea49752433
David Bogner <davidbogner@gmail.com>
2015-05-22 14:44:15 +0200

    typo fixed

--- locallib.php
+++ locallib.php
@@ -564,7 +564,7 @@ class mod_newsletter implements renderable {
         }
         
         require_once(dirname(__FILE__).'/classes/newsletter_user_subscription.php');
-        $subscriberselector = new mod_newsletter_potential_subscribersU('subsribeusers', array('newsletterid' => $this->get_instance()->id));
+        $subscriberselector = new mod_newsletter_potential_subscribers('subsribeusers', array('newsletterid' => $this->get_instance()->id));
         $subscribedusers = new mod_newsletter_existing_subscribers('subscribedusers', array('newsletterid' => $this->get_instance()->id));
         
         if(optional_param('add', false, PARAM_BOOL) && confirm_sesskey()){
